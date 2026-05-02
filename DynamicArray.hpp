00f93808db6d597bb9647807f928166a4b3bb504
Alexander <fckda3mo@MacBook-Air-Alexander.local>
2026-05-02 22:26:56 +0300

    DynamicArray Fixed

--- DynamicArray.hpp
+++ DynamicArray.hpp
@@ -19,11 +19,43 @@ class DynamicArray {
             return size;
         }
 
-        DynamicArray(const DynamicArray& other){ //используем ссылку, чтобы не копировать объект 
-            other.size = size;
-            data = new T[size]; //выделение новой памяти
+        DynamicArray(const DynamicArray& other){ //конструктор копирования
+            size = other.size;
+            data = new T[size]; 
             for (size_t i = 0; i < size; i++){
-                data[i] = other.data[i];            }
+                data[i] = other.data[i];            
             }
+        }
+
+        DynamicArray<T>& operator= (const DynamicArray& other){ //конструктор копирования присваиванием
+            delete[] data;
+            size = other.size;
+            data = new T[size]; 
+            for (size_t i = 0; i < size; i++){
+                data[i] = other.data[i];            
+            }
+            return *this;
+        }
+        
+        DynamicArray (DynamicArray&& other){ //конструктор перемещения
+            size = other.size;
+            data = other.data;
+            
+            other.size = 0;
+            other.data = nullptr;
+        }
+
+        DynamicArray<T>& operator= (DynamicArray&& other){ //конструктор перемещения присваиванием
+            delete[] data;
+            size = other.size;
+            data = other.data;
+            
+            other.size = 0;
+            other.data = nullptr;
+            return *this;
+        }
+
+        
+        
 
 };
